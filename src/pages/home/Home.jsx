fbdfe9bfa4a9c410f54135dda9f690f4d514bf74
ocircleo <salmanhossain11222626@gmail.com>
2023-10-20 09:12:09 +0600

    home page updated (cards added)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react";
 import { dataContext } from "../../private/provider/Data_Provider";
 import WelcomeSlider from "./homeCompo/welcomeSlider";
 import Catagories from "./homeCompo/Catagories";
-
+import data from "../../../public/cardData.json";
+import BaseCard from "../../shared/baseCard/BaseCard";
+import { Link } from "react-router-dom";
 const Home = () => {
   const { name } = useContext(dataContext);
   let newArray = [...new Array(4).keys()];
-
   return (
     <div className="container mx-auto mt-8">
       <WelcomeSlider></WelcomeSlider>
@@ -15,6 +16,13 @@ const Home = () => {
           <Catagories key={ele}></Catagories>
         ))}
       </div>
+      <h2 className="text-2xl underline decoration-[#6A94FF] decoration-4 underline-offset-8 text-center font-semibold">Top Rent House Around Bangladesh</h2>
+      <div className="container mx-auto grid grid-cols-1 min-[420px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-center justify-items-center gap-5 my-10 flex-wrap">
+        {data.map((ele) => (
+          <BaseCard key={ele.location} data={ele}></BaseCard>
+        ))}
+      </div>
+      <Link className="w-full bg-[#6A94FF] block text-center py-2 rounded font-semibold capitalize text-white mb-5">explore more → </Link>
     </div>
   );
 };
--- src/shared/baseCard/BaseCard.jsx
+++ src/shared/baseCard/BaseCard.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
 
-const BaseCard = () => {
-    return (
-        <div>
-            card
+const BaseCard = ({ data }) => {
+  let { location, price, type, rooms, ratings, max_people } = data;
+  return (
+    <div className="w-full min-[320px]:w-56  flex flex-col p-3 items-start rounded shadow">
+        <div className="h-36 w-full bg-gray-500 rounded mb-3">
+<img src="/" alt="" className="w-full h-36 bg-gray-500 rounded" />
         </div>
-    );
+      <p>location: {location}</p>
+      <p>
+        price: {price}tk/{type.slice(0, 2)}
+      </p>
+      <p>rooms: {rooms}</p>
+      <p>ratings: {ratings}/5</p>
+      <p>max people : {max_people}</p>
+      <Link className="bg-[#6A94FF] w-full text-center text-white font-semibold py-1 rounded mt-3 active:scale-95 duration-75">see more</Link>
+    </div>
+  );
 };
 
 export default BaseCard;
