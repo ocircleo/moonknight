f9a3c6cca0971078475a84b0fcbeb58b0c528c62
Abdur-P-Rahim <bspirahim@gmail.com>
2023-10-27 00:25:03 +0600

    statistics section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { dataContext } from "../../private/provider/Data_Provider";
 
 import Reviews from "./homeCompo/Reviews";
+import Statistics from "./homeCompo/Statistics";
 const Home = () => {
   const { name } = useContext(dataContext);
 
@@ -11,6 +12,10 @@ const Home = () => {
       <h2 className="text-2xl ps-2 font-semibold my-3">Our Top Reviews</h2>
       <div className="mb-5">
         <Reviews></Reviews>
+      </div>
+      
+      <div className="mb-5">
+        <Statistics></Statistics>
       </div>
     </div>
   );
---
+++ src/pages/home/homeCompo/Statistics.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
+
+const Statistics = () => {
+    const [counterOn, setCounterOn] = useState(false);
+    return (
+        <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+            <div className='grid lg:grid-cols-4 text-center bg-[#1E1D85]'>
+                <div className='items-center rounded-lg my-4 lg:my-12 py-10 mx-5  hover:bg-[#653cef] ease-in-out duration-1000 '>
+
+                    <img className='w-4/12 mx-auto' src="statistics/counter-icon-1 (1).svg" alt="" />
+
+                    <div className='mt-5'>
+                        <h1 className='text-white text-5xl mb-2 font-bold'>
+                            {counterOn && <CountUp start={0} end={50} duration={2} delay={0}></CountUp>}
+                            K
+                        </h1>
+                        <p className='text-white text-xl font-bold'>Listings Added</p>
+                    </div>
+                </div>
+                <div className='items-center rounded-lg my-4  lg:my-12 py-10 mx-5  hover:bg-[#653cef] ease-in-out duration-1000 '>
+
+                    <img className='w-4/12 mx-auto' src="statistics/counter-icon-2.svg" alt="" />
+
+                    <div className='mt-5'>
+                        <h1 className='text-white text-5xl mb-2 font-bold'>
+                            {counterOn && <CountUp start={0} end={3000} duration={2} delay={0}></CountUp>}
+                            +
+                        </h1>
+                        <p className='text-white text-xl font-bold'>Agents Listed</p>
+                    </div>
+                </div>
+                <div className='items-center rounded-lg my-4 lg:my-12 py-10 mx-5  hover:bg-[#653cef] ease-in-out duration-1000 '>
+
+                    <img className='w-4/12 mx-auto' src="statistics/counter-icon-3.svg" alt="" />
+
+                    <div className='mt-5'>
+                        <h1 className='text-white text-5xl mb-2 font-bold'>
+                            {counterOn && <CountUp start={0} end={2000} duration={2} delay={0}></CountUp>}
+                            +
+                        </h1>
+                        <p className='text-white text-xl font-bold'>Sales Complited</p></div>
+                </div>
+                <div className='items-center rounded-lg my-4 lg:my-12 py-10 mx-5  hover:bg-[#653cef] ease-in-out duration-1000 '>
+
+                    <img className='w-4/12 mx-auto' src="statistics/counter-icon-4.svg" alt="" />
+
+                    <div className='mt-5'>
+                        <h1 className='text-white text-5xl mb-2 font-bold'>
+                            {counterOn && <CountUp start={0} end={5000} duration={2} delay={0}></CountUp>}
+                            +
+                        </h1>
+                        <p className='text-white text-xl font-bold'>Users</p>
+                    </div>
+                </div>
+
+            </div>
+        </ScrollTrigger>
+    );
+};
+
+export default Statistics;
