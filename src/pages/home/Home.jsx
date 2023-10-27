29849881213cf129517cd67769a7d9909024b4b0
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-10-27 15:13:39 +0600

    Property section has done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,30 @@
 import { useContext, useEffect } from "react";
 import { dataContext } from "../../private/provider/Data_Provider";
 
+import Property from "./homeCompo/Property";
+
+
+
 import Reviews from "./homeCompo/Reviews";
+
+// import Reviews from "./homeCompo/Reviews";
+
 const Home = () => {
   const { name } = useContext(dataContext);
 
   return (
     <div className="container mx-auto mt-8">
-  
+
       <h2 className="text-2xl ps-2 font-semibold my-3">Our Top Reviews</h2>
       <div className="mb-5">
+        {/* <Reviews></Reviews> */}
         <Reviews></Reviews>
+        <Property></Property>
+
+
+
+
+
       </div>
     </div>
   );
