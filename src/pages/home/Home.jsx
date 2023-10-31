ec217efe27913967e56013ad956eeb0a3504a850
Abdur-P-Rahim <bspirahim@gmail.com>
2023-11-01 01:25:57 +0600

    sales section, slick carousel added, banner and others section polished

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,33 +3,28 @@ import { dataContext } from "../../private/provider/Data_Provider";
 
 import Reviews from "./homeCompo/Reviews";
 import Statistics from "./homeCompo/Statistics";
-<<<<<<< HEAD
-
-import WorkSection from "./homeCompo/WorkSection";
-import Banner from "./homeCompo/Banner";
-=======
 import Banner from "./homeCompo/Banner";
 import WorkSection from "./homeCompo/WorkSection";
 import Property from "./homeCompo/Property";
->>>>>>> e4326f253d0d7eaead89ed975b828ce38145b554
+import Card from "../../shared/Card";
+import Sales from "./homeCompo/Sales";
 const Home = () => {
   const { name } = useContext(dataContext);
 
   return (
-    <div className="container mx-auto mt-8">
+    <div className="container mx-auto">
       <div className="mb-5">
         <Banner></Banner>
       </div>
+      
       <div className="mb-5">
-        <Banner></Banner>
-      </div>
-      <div className="mb-5">
-<<<<<<< HEAD
        
       <WorkSection></WorkSection>
-=======
         <Property></Property>
->>>>>>> e4326f253d0d7eaead89ed975b828ce38145b554
+      </div>
+
+      <div className="mb-5">
+        <Sales></Sales>
       </div>
 
       <div className="mb-5">
@@ -38,6 +33,9 @@ const Home = () => {
       <h2 className="text-2xl ps-2 font-semibold my-3">Our Top Reviews</h2>
       <div className="mb-5">
         <Reviews></Reviews>
+      </div>
+      <div className="mb-5">
+        <Card></Card>
       </div>
     </div>
   );
---
+++ src/pages/home/homeCompo/Sales.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from '../../../shared/Card';
+
+const Sales = () => {
+    return (
+        <div className='my-16'>
+            <div className="text-center md:px-12 mb-10 ">
+                <h1 className="text-4xl font-bold mb-3">Featured Properties for Sales</h1>
+                <span className="w-4 h-1.5 bg-[#FD3358] inline-block rounded-2xl mr-1.5"></span>
+                <span className="w-12 h-1.5 bg-[#FD3358] inline-block rounded-2xl"></span>
+                <span></span>
+                <p className="text-lg text-slate-400 pt-4">Hand-Picked selection of quality places</p>
+            </div> 
+
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 md:px-12'>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+            </div>
+        </div>
+    );
+};
+
+export default Sales;
