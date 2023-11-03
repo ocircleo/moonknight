d5cc57d75327b9f45581fd5e653daea0ea819a62
ocircleo <salmanhossain11222626@gmail.com>
2023-11-03 07:36:41 -0700

    cities section updated

--- src/pages/home/homeCompo/Cities.jsx
+++ src/pages/home/homeCompo/Cities.jsx
@@ -4,10 +4,10 @@ import { element } from 'prop-types';
 import React from 'react';
 
 const Cities = () => {
-    let cities = ["Dhaka", "Barisal", "Jessore", "Chittagong", "Rajsashi", "Khulna",]
+    let cities = [{ city: "Dhaka", properties: '700' }, { city: "Barishal", properties: '200' }, { city: "Jessore", properties: '220' }, { city: "Kummila", properties: '170' }, { city: "Bogura", properties: '70' }, { city: "Rongpur", properties: "40" }]
 
     return (
-        <>
+        <div>
             <div className=' md:px-12 md:py-12'>
                 <div className="text-center md:px-12 mb-10 ">
                     <h1 className="text-4xl font-bold mb-3">Cities With Listing</h1>
@@ -16,26 +16,34 @@ const Cities = () => {
                     <span></span>
                     <p className="text-lg text-slate-400 pt-4">Destinations we love the most</p>
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+
+
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-4 md:px-12 p-5' data-aos="fade-down" data-aos-anchor-placement="top-center">
+
                     {
                         cities.map(ele => {
                             return (
-                                <div key={ele} className='relative w-full h-10 bg-indigo-400 rounded'>
-                                    <img src={`https://source.unsplash.com/random/240x240/?${ele}`}
-                                    className='w-full h-full absolute bg-black object-cover hover:scale-125' alt="" />
-
+                                <div key={ele.properties} className="w-auto shadow-xl h-60 bg-gray-300 relative overflow-hidden">
+                                    <div className='hover:scale-110 h-full w-full duration-200'>
+                                        <img src="https://dreamsestate.dreamstechnologies.com/html/assets/img/city/city-3.jpg" alt="" className='duration-100 absolute hover:scale-105 h-full w-full object-cover rounded z-10 to' />
+                                        <div className='h-full w-full rounded bg-gray-900/50 absolute z-20'></div>
+                                    </div>
+                                    <div className='absolute bottom-5 left-8 z-30'>
+                                        <h1 className='text-white text-2xl font-bold'>{ele.city}</h1>
+                                        <p className='text-white text-md font-bold'>{ele.properties} properties</p>
+                                    </div>
+                                    <div className='absolute  right-8 bottom-5 z-30'>
+                                        <button className="btn btn-circle bg-[#6C60FE] border-none">
+                                            <FontAwesomeIcon className=" text-black text-lg" icon={faArrowRight} />
+                                        </button>
+                                    </div>
                                 </div>
                             )
                         })
                     }
                 </div>
-
-
-
             </div>
-
-
-        </>
+        </div>
     );
 };
 
