328549cebd7bb104d2093807c32372dfbb08f36d
Iqubal905 <iqubal905@gmail.com>
2023-11-04 21:31:58 +0600

    table design

--- src/Admin/adminpage/Users.jsx
+++ src/Admin/adminpage/Users.jsx
@@ -7,7 +7,7 @@ const Users = () => {
     const [users, setUsers] = useState([]);
     
     useEffect(() => {
-        fetch('Users.json')
+        fetch('users.json')
             .then(res => res.json())
             .then(data => {
                 setUsers(data)
@@ -25,35 +25,35 @@ const Users = () => {
             <h2 className="text-4xl font-bold text-[#172554] pb-0 mb-0 text-center ">Total User :{users.length}</h2>
             <div className="divider"></div> 
             <div className="overflow-x-auto">
-  <table className="table bg-[#4F46E5] text-white">
+  <table className="table  bg-[#D5D5D5] text-black">
     
     <thead>
-      <tr className=" text-sm  bg-[#172554]  text-slate-200">
+      <tr className=" text-sm  bg-[#6366F1]  text-white">
         <th className='border'>No</th>
         <th>Img</th>
         <th>Name</th>
         <th>Email</th>
         <th>Phone Number</th>
         <th>Make Admin</th>
-        <th>Make Moderator</th>
+        <th>Make Host</th>
         <th> Delete</th>
       </tr>
     </thead>
     <tbody>
      {
-        users.map((user, index) =>  <tr key={user._id}>
-        <th className=' border text-lg'>{index + 1}</th>
+        users.map((user, index) =>  <tr key={user._id} className=' font-semibold text-base'>
+        <th className=' border text-lg bg-[#6366F1] text-white'>{index + 1}</th>
         <td><img src={user.img} className='w-14 h-14 rounded-full' alt="photo" /></td>
         <td>{user.username}</td>
         <td>{user.gmail}</td>
         <td>{user.phone_number}</td>
         <td>{user.role === 'Admin' ? 'Admin' : 
-        <button className='btn bg-[#172554]  border-none text-white  hover:bg-success'>Admin</button>
+        <button className='btn text-white  border-none  bg-indigo-400  hover:bg-success'>Admin</button>
         }</td>
         <td>{user.role === 'modarator' ? 'Instructor' : 
-        <button className='btn bg-[#172554]  border-none text-white  hover:bg-success'>Moderator</button>
+        <button className='btn bg-indigo-400  border-none text-white  hover:bg-success'> Host</button>
         }</td>
-        <td><button className='btn  bg-red-300  border-none text-white  hover:bg-red-600'>Delete</button></td>
+        <td><button className='btn  bg-red-500  border-none text-white  hover:bg-red-800'>Delete</button></td>
       </tr>)
      }
      
