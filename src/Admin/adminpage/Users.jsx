e9afb43b79e7e0d2849e932c91b52dd74a000ff7
Iqubal905 <iqubal905@gmail.com>
2023-11-05 23:32:20 +0600

    add

--- src/Admin/adminpage/Users.jsx
+++ src/Admin/adminpage/Users.jsx
@@ -1,88 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
-import { DataTable } from "primereact/datatable"
-import { Column } from "primereact/column"
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
 
-import { FilterMatchMode } from "primereact/api"
-import { InputText } from "primereact/inputtext"
-// import { filter } from 'lodash';
 
+
+  
 const Users = () => {
 
 
+
   
-const [filters, setFilters] = useState({
-  global: {value: null, matchMode: FilterMatchMode.CONTAINS },
-})
-
-
-  const people = [
-    {
-      id: 1,
-      name: "Alice",
-      age: 25,
-      city: "London"
-    },
-    {
-      id: 2,
-      name: "Bob",
-      age: 30,
-      city: "Paris"
-    },
-    {
-      id: 3,
-      name: "Charlie",
-      age: 28,
-      city: "New York"
-    },
-    {
-      id: 4,
-      name: "David",
-      age: 35,
-      city: "Tokyo"
-    },
-    {
-      id: 5,
-      name: "Eve",
-      age: 27,
-      city: "Sydney"
-    },
-    {
-      id: 6,
-      name: "Frank",
-      age: 40,
-      city: "Berlin"
-    },
-    {
-      id: 7,
-      name: "Grace",
-      age: 22,
-      city: "Barcelona"
-    },
-    {
-      id: 8,
-      name: "Hannah",
-      age: 29,
-      city: "Rome"
-    },
-    {
-      id: 9,
-      name: "Isaac",
-      age: 33,
-      city: "Toronto"
-    },
-    {
-      id: 10,
-      name: "Jack",
-      age: 26,
-      city: "Madrid"
-    }
-  ];
-  
-
-
 
 
 
@@ -103,80 +29,42 @@ const [filters, setFilters] = useState({
 
 
 
-const imageBodyTemplate = (users) => {
-  return <img src={users.img} alt="User" className='w-14 h-14  rounded-full' />;
-};
 
-const indexBodyTemplate = (rowData, props) => {
-  return (
-    <span className=' text-lg font-bold'>{props.rowIndex + 1}</span>
-  );
-};
-
-
-const adminTemplate = () => {
-  return (
-    <button className='btn text-white border-none bg-indigo-400 hover:bg-success'>Admin</button>
-  );
-};
-
-const hostTemplate = () => {
-  return (
-    <button className='btn bg-indigo-400 border-none text-white hover:bg-success'>Host</button>
-  );
-};
-
-const deleteTemplate = () => {
-  return (
-    <button className='btn bg-red-500 border-none text-white hover:bg-red-800'>Delete</button>
-  );
-};
 
 
 console.log(users);
-console.log(people);
+
     return (
         <div>
- <h2 className="text-2xl font-bold text-[#172554] pb-0 mb-0 text-center ">Total User :{users.length}</h2>
-           
 
- <InputText
- className='  border-2 p-1  w-96    border-[#6366F1]'
- placeholder='Search by name, gmail, phone number'
-    onInput={(e) =>
-        setFilters({
-          global: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS },
-        })
-    }
- />
+         <div>
+   
+            <h2 className="text-2xl font-bold text-[#172554] pb-0 mb-0 text-center border-y-2 ">Total User :{users.length}</h2>
 
-
-
-<DataTable value={users} sortMode='multiple' filters={filters}
-paginator
-rows={5}
-rowsPerPageOptions={[1,2,3,4,5]}
-totalRecords={5}
->
-
-<Column header="No" body={indexBodyTemplate} style={{textAlign: 'center'}} />
-<Column field="imageURL" header="Image"  body={imageBodyTemplate} />
-    
-      <Column field='username' header="Name" sortable/> 
-      <Column field='gmail' header="Gmail" sortable/> 
-      <Column field='phone_number' header="Phone number" sortable /> 
-      <Column header="Admin" body={adminTemplate} />
-      <Column header="Host" body={hostTemplate} />
-      <Column header="Delete" body={deleteTemplate} />
-</DataTable>
+ <div className='flex'>
+ <div className="flex items-center bg-white rounded-lg shadow-sm  w-72 h-11 border-2 ">
+  <input type="text" className=" py-0 my-0 px-4 rounded-lg focus:outline-none" placeholder="Search..."/>
+  <button className=" w-full flex items-center justify-center h-full px-4">
+  <svg className="pointer-events-none absolute z-10 my-3.5 ml-4 stroke-current opacity-60 text-base-content" width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+  </button>
+     </div>
+ <div className="dropdown dropdown-hover m-0 ">
+  <label tabIndex={0} className="btn btn-sm h-6  bg-indigo-500  text-white hover:bg-success  text-xs  m-1">Search by
+  
+  <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-6 text-white  " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+</svg>
+  </label>
+  <ul tabIndex={0} className="dropdown-content z-[1] bg-success text-white menu p-2 shadow  rounded-box w-52">
+    <li><a>Gmail</a></li>
+    <li><a>Phone Number</a></li>
+  </ul>
+</div>
+ </div>
 
 
-
-
-
-             {/* <div>
-            <h2 className="text-4xl font-bold text-[#172554] pb-0 mb-0 text-center ">Total User :{users.length}</h2>
-            <div className="divider"></div> 
+            {/* <div className="divider mt-0 mb-1"></div>  */}
             <div className="overflow-x-auto">
   <table className="table  bg-[#D5D5D5] text-black">
     
@@ -201,10 +89,10 @@ totalRecords={5}
         <td>{user.gmail}</td>
         <td>{user.phone_number}</td>
         <td>{user.role === 'Admin' ? 'Admin' : 
-        <button className='btn text-white  border-none  bg-indigo-400  hover:bg-success'>Admin</button>
+        <button className='btn text-white  border-none  bg-indigo-500  hover:bg-success'>Admin</button>
         }</td>
         <td>{user.role === 'Host' ? 'Host' : 
-        <button className='btn bg-indigo-400  border-none text-white  hover:bg-success'> Host</button>
+        <button className='btn bg-indigo-500  border-none text-white  hover:bg-success'> Host</button>
         }</td>
         <td><button className='btn  bg-red-500  border-none text-white  hover:bg-red-800'>Delete</button></td>
       </tr>)
@@ -214,7 +102,16 @@ totalRecords={5}
     </tbody>
   </table>
 </div>
-        </div>  */}
+        </div> 
+   <div className=' flex justify-center  bg-slate-400'>
+   <div className="join ">
+  <button className="join-item btn">1</button>
+  <button className="join-item btn">2</button>
+  <button className="join-item btn ">...</button>
+  <button className="join-item btn">99</button>
+  <button className="join-item btn">100</button>
+</div>
+   </div>
         </div>
     );
 };
