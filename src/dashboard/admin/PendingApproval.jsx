f8eeac33f21969f5071f3dc2d1fbcdb38e571584
Iqubal905 <iqubal905@gmail.com>
2023-11-06 23:01:17 +0600

    approvePageAdd

--- src/dashboard/admin/PendingApproval.jsx
+++ src/dashboard/admin/PendingApproval.jsx
@@ -1,35 +1,76 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-const url = "../../../public/Users.json"
+const url = "../../../public/users.json"
 
 const PendingApproval = () => {
-    const [user, setUser] = useState([]);
+    const [users, setUsers] = useState([]);
+
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
-            .then(data => setUser(data))
+            .then(data => setUsers(data))
+            
     }, [])
-
+console.log(users);
 
     return (
-        <div>
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 px-4 py-8">
-                {
-                    user.map(user => <div key={user.index}>
-                        <div className="card card-side bg-base-100 shadow-xl sm:h-[300px]">
-                            <figure><img className="h-24 rounded" src={user.image} alt="Movie" /></figure>
-                            <div className="card-body flex flex-col justify-between">
-                                <h2 className="card-title text-2xl font-bold px-4 py-4">Name: {user.title}</h2>
-                                <div className="flex gap-2 justify-end">
-                                    <button className="btn btn-outline btn-success">verify</button>
-                                    <button className="btn btn-outline btn-error">Error</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>)
-                }
-            </div>
+        <div className=" bg-gray-400">
+           <div className="overflow-x-auto">
+           <h2 className="text-xl font-bold text-[#172554] pb-1 mb-0 text-center border-y-2  ">Pending Users for approval :{users.length}</h2>
+
+           <table className="table">
+    {/* head */}
+    <thead>
+    <tr className=" text-sm  bg-[#6366F1]  text-white">
+        <th className="pr-0 mr-0 border w-16">No</th>
+        <th className=" text-center">Image</th>
+        <th className=" text-center">Title</th>
+        <th className=" text-center">Approve</th>
+        <th className=" ">Deny</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+         users.map((user, index)=>  <tr  key={user._id}  className=' font-semibold text-base  border-2'>
+      <th className=' border text-lg  text-center  text-white'>{index + 1}</th>
+          <td>
+           
+              <div className="avatar   flex justify-center">
+                <div className="mask mask-squircle w-12 h-12">
+                  <img src={user.img} alt="Avatar Tailwind CSS Component" />
+                </div>
+              </div>
+             
+     
+          </td>
+        
+          <td className=" text-center  text-slate-950  font-semibold">{user.username}</td>
+          <th className="  flex justify-center">
+            <button className="btn    text-white bg-indigo-500 btn-  border-0 hover:bg-indigo-900">Approve</button>
+          </th>
+          <th  className=" ">
+            <button className="btn   text-white bg-red-500 btn-  border-0 hover:bg-red-900">Deny</button>
+          </th>
+        </tr>
+         
+         )
+
+      }
+
+     
+    </tbody>
+ 
+   </table>
+   
+<div className="flex justify-center   mt-4 ">
+<div className="join">
+  <button className="join-item btn border-indigo-500 hover:border-indigo-800 hover:border-y-4">«</button>
+  <button className="join-item btn border-y-2 border-indigo-500 hover:border-indigo-800   hover:border-y-4">Page 22</button>
+  <button className="join-item btn border-indigo-500  hover:border-indigo-800 hover:border-y-4">»</button>
+</div>
+</div>
+</div> 
         </div>
     );
 };
--- src/dashboard/admin/Users.jsx
+++ src/dashboard/admin/Users.jsx
@@ -1,113 +1,98 @@
+import React, { useEffect, useState } from 'react';
 
+  
 const Users = () => {
-  const people = [
-    {
-      id: 1,
-      name: "Alice",
-      age: 25,
-      city: "London"
-    },
-    {
-      id: 2,
-      name: "Bob",
-      age: 30,
-      city: "Paris"
-    },
-    {
-      id: 3,
-      name: "Charlie",
-      age: 28,
-      city: "New York"
-    },
-    {
-      id: 4,
-      name: "David",
-      age: 35,
-      city: "Tokyo"
-    },
-    {
-      id: 5,
-      name: "Eve",
-      age: 27,
-      city: "Sydney"
-    },
-    {
-      id: 6,
-      name: "Frank",
-      age: 40,
-      city: "Berlin"
-    },
-    {
-      id: 7,
-      name: "Grace",
-      age: 22,
-      city: "Barcelona"
-    },
-    {
-      id: 8,
-      name: "Hannah",
-      age: 29,
-      city: "Rome"
-    },
-    {
-      id: 9,
-      name: "Isaac",
-      age: 33,
-      city: "Toronto"
-    },
-    {
-      id: 10,
-      name: "Jack",
-      age: 26,
-      city: "Madrid"
-    }
-  ];
-
-  return (
-    <div>
-      <div>
-        <h2 className="text-4xl font-bold text-[#172554] pb-0 mb-0 text-center ">Total User :{people.length}</h2>
-        <div className="divider"></div>
-        <div className="overflow-x-auto">
-          <table className="table  bg-[#D5D5D5] text-black">
-
-            <thead>
-              <tr className=" text-sm  bg-[#6366F1]  text-white">
-                <th className='border'>No</th>
-                <th>Img</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone Number</th>
-                <th>Make Admin</th>
-                <th>Make Host</th>
-                <th> Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                people.map((user, index) => <tr key={user._id} className=' font-semibold text-base'>
-                  <th className=' border text-lg bg-[#6366F1] text-white'>{index + 1}</th>
-                  <td><img src={user.img} className='w-14 h-14 rounded-full' alt="photo" /></td>
-                  <td>{user.username}</td>
-                  <td>{user.gmail}</td>
-                  <td>{user.phone_number}</td>
-                  <td>{user.role === 'Admin' ? 'Admin' :
-                    <button className='btn text-white  border-none  bg-indigo-400  hover:bg-success'>Admin</button>
-                  }</td>
-                  <td>{user.role === 'Host' ? 'Host' :
-                    <button className='btn bg-indigo-400  border-none text-white  hover:bg-success'> Host</button>
-                  }</td>
-                  <td><button className='btn  bg-red-500  border-none text-white  hover:bg-red-800'>Delete</button></td>
-                </tr>)
-              }
 
 
-            </tbody>
-          </table>
+    const [users, setUsers] = useState([]);
+    
+    useEffect(() => {
+        fetch('users.json')
+            .then(res => res.json())
+            .then(data => {
+                setUsers(data)
+                console.log(data)
+                
+            });
+    }, [])
+
+console.log(users);
+
+    return (
+        <div>
+
+         <div>
+   
+            <h2 className="text-2xl font-bold  text-[#172554] pb-0 mb-0 text-center border-y-2 ">Total User :{users.length}</h2>
+
+ <div className='flex'>
+ <div className="flex items-center bg-white rounded-lg shadow-sm  w-72 h-11 border-2 ">
+  <input type="text" className=" py-0 my-0 px-4 rounded-lg focus:outline-none" placeholder="Search..."/>
+  <button className=" w-full flex items-center justify-center h-full px-4">
+  <svg className="pointer-events-none absolute z-10 my-3.5 ml-4 stroke-current opacity-60 text-base-content" width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+  </button>
+     </div>
+ <div className="dropdown dropdown-hover m-0 ">
+  <label tabIndex={0} className="btn btn-sm h-6  bg-indigo-500  text-white hover:bg-success  text-xs  m-1">Search by
+  
+  <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-6 text-white  " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+</svg>
+  </label>
+  <ul tabIndex={0} className="dropdown-content z-[1] bg-success text-white menu p-2 shadow  rounded-box w-52">
+    <li><a>Gmail</a></li>
+    <li><a>Phone Number</a></li>
+  </ul>
+</div>
+ </div>
+ <div className="overflow-x-auto">
+  <table className="table  bg-gray-400 text-black">
+    
+    <thead>
+      <tr className=" text-sm  bg-[#6366F1]  text-white">
+        <th className='border'>No</th>
+        <th>Img</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone Number</th>
+        <th>Make Admin</th>
+        <th>Make Host</th>
+        <th> Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+     {
+        users.map((user, index) =>  <tr key={user._id} className=' font-semibold text-base border-2'>
+        <th className=' border text-lg border-r-2 text-white'>{index + 1}</th>
+        <td><img src={user.img} className='w-14 h-14 rounded-full' alt="photo" /></td>
+        <td>{user.username}</td>
+        <td>{user.gmail}</td>
+        <td>{user.phone_number}</td>
+        <td>{user.role === 'Admin' ? 'Admin' : 
+        <button className='btn text-white  border-none  bg-indigo-500  hover:bg-success'>Admin</button>
+        }</td>
+        <td>{user.role === 'Host' ? 'Host' : 
+        <button className='btn bg-indigo-500  border-none text-white  hover:bg-success'> Host</button>
+        }</td>
+        <td><button className='btn  bg-red-500  border-none text-white  hover:bg-red-800'>Delete</button></td>
+      </tr>)
+     }
+      </tbody>
+  </table>
+</div>
+        </div> 
+   <div className=' flex justify-center  bg-slate-400'>
+   <div className="join ">
+  <button className="join-item btn">1</button>
+  <button className="join-item btn">2</button>
+  <button className="join-item btn ">...</button>
+  <button className="join-item btn">99</button>
+  <button className="join-item btn">100</button>
+</div>
+   </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Users;
