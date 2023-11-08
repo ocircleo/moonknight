424f949f195d365f9e0421e655f622ffba379adb
ocircleo <salmanhossain11222626@gmail.com>
2023-11-08 11:40:05 +0600

    space added

--- src/pages/beAhost/PostARent.jsx
+++ src/pages/beAhost/PostARent.jsx
@@ -2,7 +2,7 @@
 
 const PostARent = () => {
     const submit = e => {
-            e.preventDefault()
+        e.preventDefault()
         const form = e.target;
         const title = form.title.value;
         const price = form.price.value;
@@ -13,29 +13,28 @@ const PostARent = () => {
         const beds = form.beds.value;
         const bathroom = form.bathroom.value;
         const garage = form.garage.value;
-        const builtin = form.builtin.value;
         const floor = form.floor.value;
-        const maxpepol = form.maxpepol.value;
+        const maxPeople = form.maxpepol.value;
         const description = form.description.value;
-        console.log(title, price, city, region, area, rooms, beds, bathroom, garage, builtin, floor, maxpepol, description)
+        const space = form.space.value
+        console.log(title, price, city, region, area, rooms, beds, bathroom, garage, floor, description,space)
 
         const uploadHome = {
-            title,
-            price,
-            city,
-            region,
-            area,
-            rooms,
-            beds,
-            bathroom,
-            garage,
-            builtin,
-            email,
-            floor,
-            maxpepol,
-            description
+            title: title,
+            price: price,
+            city: city,
+            region: region,
+            area: area,
+            rooms: rooms,
+            beds: beds,
+            bathroom: bathroom,
+            garage: garage || 'not provided',
+            floor: floor || 'not provided',
+            maxPeople: maxPeople,
+            description: description,
+            space: space,
         }
-        fetch("", {
+        fetch("http://localhost:3000/host/postHouse", {
             method: "POST",
             headers: {
                 "content-type": "application/json"
@@ -83,7 +82,7 @@ const PostARent = () => {
                             </fieldset>
                         </div>
                         <fieldset className='flex flex-col gap-3'>
-                            <label className='font-semibold text-xl'>Area</label>
+                            <label className='font-semibold text-xl'>Area </label>
                             <input type="text" name='area' placeholder="Area" className="input input-bordered bg-slate-100 input-info w-full lg:w-[490px] md:w-[490px] block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                             <p className='text-sm text-red-500'></p>
                         </fieldset>
@@ -112,7 +111,7 @@ const PostARent = () => {
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                         </div>
-                    
+
                         <div className='lg:flex md:flex lg:gap-6 md:gap-6'>
                             <fieldset className='flex flex-col gap-3'>
                                 <label className='font-semibold text-xl'>Which Floor ?</label>
@@ -125,16 +124,18 @@ const PostARent = () => {
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                         </div>
-
+                        <fieldset className='flex flex-col gap-3'>
+                            <label className='font-semibold text-xl'>Space ( sqft )</label>
+                            <input type="number" name='space' placeholder="ex 2000 sqft" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
+                            <p className='text-sm text-red-500'></p>
+                        </fieldset>
                         <fieldset className='flex flex-col gap-3'>
                             <label className='font-semibold text-xl'>Description</label>
-                            <textarea name='description' className="textarea textarea-info input input-bordered bg-slate-100 input-info w-full lg:w-[490px] h-20 block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" placeholder="Enter Description"></textarea>
+                            <textarea name='description' required className="textarea textarea-info input input-bordered bg-slate-100 input-info w-full lg:w-[490px] h-20 block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" placeholder="Enter Description"></textarea>
                             <p className='text-sm text-red-500'></p>
                         </fieldset>
 
-                        <input type='submit' className="btn glass bg-indigo-600 text-white hover:btn glass" value="Submit" />
-
-
+                        <input type='submit' className="btn  bg-indigo-600 text-white hover:btn glass" value="Submit" />
                     </div>
 
                 </form>
