4c2da80afcea76cbffc557374a6778b2984936bb
ocircleo <salmanhossain11222626@gmail.com>
2023-11-08 11:18:44 +0600

    post house info aded

--- src/pages/beAhost/PostARent.jsx
+++ src/pages/beAhost/PostARent.jsx
@@ -2,7 +2,7 @@
 
 const PostARent = () => {
     const submit = e => {
-        e.preventDefault()
+            e.preventDefault()
         const form = e.target;
         const title = form.title.value;
         const price = form.price.value;
@@ -14,11 +14,10 @@ const PostARent = () => {
         const bathroom = form.bathroom.value;
         const garage = form.garage.value;
         const builtin = form.builtin.value;
-        const email = form.email.value;
         const floor = form.floor.value;
         const maxpepol = form.maxpepol.value;
         const description = form.description.value;
-        console.log(title, price, city, region, area, rooms, beds, bathroom, garage, builtin, email, floor, maxpepol, description)
+        console.log(title, price, city, region, area, rooms, beds, bathroom, garage, builtin, floor, maxpepol, description)
 
         const uploadHome = {
             title,
@@ -50,27 +49,23 @@ const PostARent = () => {
     }
     return (
         <div className='w-full lg:w-5/6 mx-auto p-2 mt-10'>
-
-
             <section className='bg-white  rounded px-8 pt-6 pb-8 mb-4 lg:w-[550px] '>
-
                 <div className='pb-5'>
                     <h1 className='text-indigo-500 font-semibold text-xl pb-2 lg:text-3xl  capitalize'> Rent House Add</h1>
                     <span className="w-14 h-1.5 bg-indigo-600 inline-block rounded-2xl mr-1.5"></span>
                     <span className="w-40 h-1.5 bg-indigo-600 inline-block rounded-2xl"></span>
                 </div>
-
                 <form onSubmit={submit}>
                     <div className='flex flex-col gap-3 ' >
                         <div className='lg:flex md:flex lg:gap-6 md:gap-6'>
                             <fieldset className='flex flex-col gap-3'>
                                 <label className='font-semibold text-xl'>Title</label>
-                                <input type="text" name='title' placeholder="Title" className="input input-bordered bg-slate-100 input-info max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
+                                <input type="text" required name='title' placeholder="Title" className="input input-bordered bg-slate-100 input-info max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                             <fieldset className='flex flex-col gap-3'>
                                 <label className='font-semibold text-xl'>Price</label>
-                                <input type="number" name='price' placeholder="Price" className="input input-bordered bg-slate-100 input-info max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
+                                <input type="number" required name='price' placeholder="Price" className="input input-bordered bg-slate-100 input-info max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                         </div>
@@ -78,12 +73,12 @@ const PostARent = () => {
                         <div className='lg:flex md:flex lg:gap-6 md:gap-6'>
                             <fieldset className='flex flex-col gap-3'>
                                 <label className='font-semibold text-xl'>City</label>
-                                <input type="text" name='city' placeholder="City" className="input input-bordered bg-slate-100 input-info max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
+                                <input type="text" required name='city' placeholder="City" className="input input-bordered bg-slate-100 input-info max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                             <fieldset className='flex flex-col gap-3'>
                                 <label className='font-semibold text-xl'>Region</label>
-                                <input type="text" name='region' placeholder="Region" className="input input-bordered bg-slate-100 input-info max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
+                                <input type="text" required name='region' placeholder="Region" className="input input-bordered bg-slate-100 input-info max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                         </div>
@@ -95,20 +90,20 @@ const PostARent = () => {
 
                         <div className='lg:flex md:flex lg:gap-6 md:gap-6'>
                             <fieldset className='flex flex-col gap-3'>
-                                <label className='font-semibold text-xl'>Rooms</label>
-                                <input type="number" name='rooms' placeholder="Rooms" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
+                                <label className='font-semibold text-xl'>Total Rooms</label>
+                                <input type="number" required name='rooms' placeholder="Rooms" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                             <fieldset className='flex flex-col gap-3'>
-                                <label className='font-semibold text-xl'>Beds</label>
-                                <input type="number" name='beds' placeholder="Beds" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
+                                <label className='font-semibold text-xl'>Beds Room</label>
+                                <input type="number" required name='beds' placeholder="Beds" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                         </div>
                         <div className='lg:flex md:flex lg:gap-6 md:gap-6'>
                             <fieldset className='flex flex-col gap-3'>
                                 <label className='font-semibold text-xl'>Bathroom</label>
-                                <input type="number" name='bathroom' placeholder="Bathroom" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
+                                <input type="number" required name='bathroom' placeholder="Bathroom" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                             <fieldset className='flex flex-col gap-3'>
@@ -117,26 +112,15 @@ const PostARent = () => {
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                         </div>
+                    
                         <div className='lg:flex md:flex lg:gap-6 md:gap-6'>
                             <fieldset className='flex flex-col gap-3'>
-                                <label className='font-semibold text-xl'>BuiltIn</label>
-                                <input type="text" name='builtin' placeholder="BuiltIn" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
-                                <p className='text-sm text-red-500'></p>
-                            </fieldset>
-                            <fieldset className='flex flex-col gap-3'>
-                                <label className='font-semibold text-xl'>HostEmail</label>
-                                <input type="email" name='email' placeholder="HostEmail" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
-                                <p className='text-sm text-red-500'></p>
-                            </fieldset>
-                        </div>
-                        <div className='lg:flex md:flex lg:gap-6 md:gap-6'>
-                            <fieldset className='flex flex-col gap-3'>
-                                <label className='font-semibold text-xl'>Floor</label>
+                                <label className='font-semibold text-xl'>Which Floor ?</label>
                                 <input type="number" name='floor' placeholder="Floor" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
                             <fieldset className='flex flex-col gap-3'>
-                                <label className='font-semibold text-xl'>MaxPeople</label>
+                                <label className='font-semibold text-xl'>Max People</label>
                                 <input type="number" name='maxpepol' placeholder="MaxPeople" className="input input-bordered bg-slate-100 input-info w-full max-w-xs block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" />
                                 <p className='text-sm text-red-500'></p>
                             </fieldset>
@@ -147,10 +131,10 @@ const PostARent = () => {
                             <textarea name='description' className="textarea textarea-info input input-bordered bg-slate-100 input-info w-full lg:w-[490px] h-20 block pr-10 shadow appearance-none border-b-2 border-white border-b-indigo-400  rounded w-full py-2 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-b-indigo-900 transition duration-500 ease-in-out outline-none" placeholder="Enter Description"></textarea>
                             <p className='text-sm text-red-500'></p>
                         </fieldset>
-                        
+
                         <input type='submit' className="btn glass bg-indigo-600 text-white hover:btn glass" value="Submit" />
-                            
-                        
+
+
                     </div>
 
                 </form>
