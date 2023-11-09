47c4fb7316a1c0f606d2d849ba808c0a0f371a97
ocircleo <salmanhossain11222626@gmail.com>
2023-11-09 11:24:42 +0600

    page updated

--- src/dashboard/compo/Whishlist.jsx
+++ src/dashboard/compo/Whishlist.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Authcontext } from '../../private/provider/Provider';
 
 const Whishlight = () => {
+    const { userDB } = useContext(Authcontext)
+    console.log(userDB)
     return (
         <div>
             Whishlight
