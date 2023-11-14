c44a1995bf78c1fe3bd68371ff38bbded0a4578d
ocircleo <salmanhossain11222626@gmail.com>
2023-11-14 19:44:22 +0600

    show detaill page updated

--- src/dashboard/compo/Notifications.jsx
+++ src/dashboard/compo/Notifications.jsx
@@ -1,53 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
-import MailCompo from '../compo/MailCompo';
 import NotifiCompo from './NotifiCompo';
 import usePageTitle from '../../hooks/PageTitleHook';
+import { Authcontext } from '../../private/provider/Provider';
 
 const Notifications = () => {
-
+  const { userDB } = useContext(Authcontext)
   usePageTitle('Too late | notifications');
 
-    const [allUsers, setAllUsers] = useState([]);
-    useEffect(() => {
-      fetch('https://moonknight-backend.vercel.app/admin/getUsers/all')
+  const [allUsers, setAllUsers] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:3000/user/myMessage/${userDB.email}`)
+      .then(res => res.json())
+      .then(data => {
+        setAllUsers(data)
+      });
+  }, [userDB])
+
+  const handleDelete = id => {
+    const confirmation = confirm('Are you sure you want to delete');
+    if (confirmation) {
+      fetch(`https://moonknight-backend.vercel.app/deleteEmail/${id}`, {
+        method: 'DELETE'
+      })
         .then(res => res.json())
         .then(data => {
-          setAllUsers(data)
-        });
-    }, [])
+          console.log(data);
+          if (data.deletedCount > 0) {
+            toast('uploaded successfully')
+          }
+        })
+    }
+  }
 
-    const handleDelete = id => {
-      const confirmation = confirm('Are you sure you want to delete');
-     if(confirmation){
-           fetch(`https://moonknight-backend.vercel.app/deleteEmail/${id}`, {
-              method: 'DELETE'
-           })
-           .then(res => res.json())
-           .then(data => {
-              console.log(data);
-              if(data.deletedCount > 0){
-                toast('uploaded successfully')
-                  }
-           })
-     }
-   }
-   
-    return (
-        <div className=' bg-slate-50'>
- <h2 className="text-xl font-bold  bg-slate-100 text-[#172554] pb-0 mb-0 text-center border-y-2 ">Total Notification : {allUsers.length}</h2>
-    {
-        allUsers.map((user) =><NotifiCompo
-       
+  return (
+    <div className=''>
+      {allUsers.length > 0 ? (
+        allUsers.map((user) => <NotifiCompo
+
           key={user._id}
           user={user}
           handleDelete={handleDelete}
-         >
-         </NotifiCompo>
-         )
-    }
- </div>
-    );
+        >
+        </NotifiCompo>
+        )) : <h1 className='text-center text-xl font-bold py-8 capitalize text-gray-400'>no notification</h1>
+      }
+    </div>
+  );
 };
 
 export default Notifications;
